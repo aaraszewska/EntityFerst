25c7e6993fdc52f1b9bdda9a196932dd3ca75022
Anna <aaraszewska@gmail.com>
2019-03-24 20:58:35 +0000

    first commit

---
+++ EntityFirst/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using EntityFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult CreateNewEmployee(Employee employee)
+        {
+            EmployeeDbContext db = new EmployeeDbContext();
+            db.Employees.Add(employee);
+            db.SaveChanges();
+
+            return View("Index");
+        }
+
+    }
+}
---
+++ EntityFirst/Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EntityFirst.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmployeeId { get; set; }
+        [Display(Name = "First Name:")]
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstNmae { get; set; }
+        [Display(Name = "Last Name:")]
+        [Required(ErrorMessage = "Last  Name is required")]
+        public string LastMane { get; set; }
+        [Display(Name = "Email Address:")]
+        [Required(ErrorMessage = "Email address is required")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+        [Display(Name = "Phone Number:")]
+        [Required(ErrorMessage = "Phone Number  is required")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = " Address:")]
+        [Required(ErrorMessage = "address is required")]
+        public string Address { get; set; }
+        public Nullable<int> Age { get; set; }
+        [Display(Name = "Salary:")]
+        [Required(ErrorMessage = "Salary is required")]
+        public Nullable<decimal> Salary { get; set; }
+        public string Worktype { get; set; }
+        public bool UseCarPark { get; set; }
+    }
+}
